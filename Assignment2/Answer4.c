b6344d19d51f7e5fd778e2e9e3ff7f2f0598f24b
Tushar Gupta <tushar158gupta@gmail.com>
2022-09-25 17:25:02 +0530

    Update Answer4.c
    felt much more easy . but increased space complexity

--- Assignment2/Answer4.c
+++ Assignment2/Answer4.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
-
+ 
 int max_of_four(int a, int b, int c, int d){
- int lmao = a;
- if(a>b){
-     lmao = a;
-     if(c>a){
-         lmao =c;
-        if (d>c) {
-        lmao =d;
-        }
-     }
- }
- else {
- lmao = b;
-     if(c>b){
-         lmao =c;
-        if (d>c) {
-        lmao =d;
-        }
-     }
- }
- return lmao;   
+ int lmao1 ;
+ if(a<b)lmao1 =b;
+ else lmao1 = a;
+ int lmao2;
+ if(c<d)lmao2 =d;
+ else lmao2 = c;
+ 
+ if(lmao2<lmao1)return lmao1;
+ else return lmao2;
+ 
+ 
 }
-
-
+ 
+ 
 int main() {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
     printf("%d", ans);
-    
+ 
     return 0;
 }
